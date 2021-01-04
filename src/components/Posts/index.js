284a1736ab57ba53d1a1622164a8bcbb395a269c
geoffreyhale <geoffreyhale@gmail.com>
2021-01-04 11:07:26 -0800

    limit posts column width for readability

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -309,7 +309,7 @@ export default class Posts extends Component {
     return (
       <Row>
         <Col></Col>
-        <Col sm={8}>
+        <Col sm={8} bsClass="col-posts">
           <Card>
             <Card.Body>
               <Card.Title>
