f26cc854480462f5ce69c03c88ee00e96d414146
geoffreyhale <geoffreyhale@gmail.com>
2021-01-15 10:39:38 -0800

    add firestore to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,8 +3,12 @@ const config = {
   apiKey: 'AIzaSyC301BeuAVDII84J7K0mq7AUJjUMgwRWYo',
   authDomain: 'react-firebase-2020-aecbd.firebaseapp.com',
   databaseURL: 'https://react-firebase-2020-aecbd-default-rtdb.firebaseio.com',
+  projectId: 'react-firebase-2020-aecbd',
 };
 firebase.initializeApp(config);
+
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
 export default firebase;
+
+export const db = firebase.firestore();
