b4c7433829b7dea08a4f139504450dd03e8ef466
geoffreyhale <geoffreyhale@gmail.com>
2021-01-15 16:22:05 -0800

    TemporarilyDownForMaintainence

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const TemporarilyDownForMaintainence = () => (
+  <>Site temporarily down for maintenance. Brb, friends!</>
+);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
