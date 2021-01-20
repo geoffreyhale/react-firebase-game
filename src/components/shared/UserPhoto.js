1257c2ca9b13a75c39fee382a50e7d29bdda43db
geoffreyhale <geoffreyhale@gmail.com>
2021-01-20 10:59:33 -0800

    add PremiumIcon to UserPhoto

--- src/components/shared/UserPhoto.js
+++ src/components/shared/UserPhoto.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
-const UserPhoto = ({ src, size = 48 }) => (
-  <img src={src} alt="user" style={{ height: size }} />
+const PremiumIcon = ({ size }) => (
+  <div
+    style={{
+      position: 'absolute',
+      left: size / 48,
+      top: 0,
+      fontSize: size / 3,
+    }}
+    title="premium"
+  >
+    &#11088; {/* white medium star, emoji */}
+  </div>
 );
+
+const UserPhoto = ({ src, size = 48, premium = false }) => {
+  return (
+    <div style={{ position: 'relative', display: 'inline-block' }}>
+      <img src={src} alt="user" style={{ height: size }} />
+      {premium && <PremiumIcon size={size} />}
+    </div>
+  );
+};
 export default UserPhoto;
