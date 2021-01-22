90d3efd879a2b16431dcbbab5e0cb786fbabf68f
geoffreyhale <geoffreyhale@gmail.com>
2021-01-21 17:08:02 -0800

    shared Spinner

---
+++ src/components/shared/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Spinner = () => (
+  <Spinner animation="border" role="status" variant="primary">
+    <span className="sr-only">Loading...</span>
+  </Spinner>
+);
+
+export default Spinner;
