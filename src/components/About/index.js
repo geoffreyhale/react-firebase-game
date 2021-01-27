18c3bceaeecdebf56fd5dd4f17b51faab4ed7a89
geoffreyhale <geoffreyhale@gmail.com>
2021-01-27 15:06:00 -0800

    improve About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,9 +12,20 @@ const About = () => (
         <Card.Body>
           <h2>What is xBook?</h2>
           <p>
-            <a href="https://xbk.io/">xBook</a> is the social network for
-            healthy relating.
+            <Link to="/" style={{ color: 'inherit', fontWeight: '500' }}>
+              xBook
+            </Link>{' '}
+            is a new online social network for{' '}
+            <Link
+              to="/r/healthyrelating"
+              style={{ color: 'inherit', fontWeight: '500' }}
+            >
+              healthy relating
+            </Link>
+            .
           </p>
+          <p>xBook focuses on safety and meaningful connection.</p>
+          {/* <p>xBook will never sell your data.</p> */}
           {/* <p>
             We value respectful, considerate participation. We value rationality
             and empathy. We welcome all ideas and feelings.
@@ -28,41 +39,43 @@ const About = () => (
           </p>
           <p>
             Join premium for $1/mo and join us in{' '}
-            <Link to="/r/healthyrelating">r/healthyrelating</Link>
+            <Link to="/r/healthyrelating">r/healthyrelating</Link>.
           </p>
-          <h2>How do I pay?</h2>
-          {/* <p>Premium access is currently offered for $1/mo.</p> */}
+          {/* <h2>How do I pay?</h2>
           <p>
-            Ask us anything in <Link to="/r/general">r/general</Link>.
-          </p>
+            Log in and ask us anything in <Link to="/r/general">r/general</Link>
+            .
+          </p> */}
           <h2>Who are we?</h2>
           <p>
             We build an online social networking platform for healthy relating.
           </p>
           <p>
             We were founded in 2020 by{' '}
-            <Link to="/u/GS6qQS0bCMWbeonwBAauvjlCsen1">Geoffrey Hale</Link>, an
-            engineer passionate about healthy relating.
+            <Link
+              to="/u/GS6qQS0bCMWbeonwBAauvjlCsen1"
+              style={{ color: 'inherit' }}
+            >
+              Geoffrey Hale
+            </Link>
+            , an engineer passionate about healthy relating.
           </p>
-          <p>
+          {/* <p>
             If you'd like to know more you can ask us anything in{' '}
             <Link to="/r/general">r/general</Link>.
-          </p>
+          </p> */}
           <h2>What is it built with?</h2>
           <p>
             It is a JavaScript application that runs in your web browser, using
-            the{' '}
-            <a href="https://reactjs.org/" target="_blank">
-              React
-            </a>{' '}
-            framework.
+            the {/* <a href="https://reactjs.org/" target="_blank"> */}
+            React
+            {/* </a> */} framework.
           </p>
           <p>
             Backend concerns are handled with{' '}
-            <a href="https://firebase.google.com/" target="_blank">
-              Firebase
-            </a>{' '}
-            Realtime Database and Cloud Firestore.
+            {/* <a href="https://firebase.google.com/" target="_blank"> */}
+            Firebase
+            {/* </a> */} Realtime Database and Cloud Firestore.
           </p>
         </Card.Body>
       </Card>
