7c7cf3e7df2fe827c0b179da9b3190462472b94c
geoffreyhale <geoffreyhale@gmail.com>
2021-01-27 13:13:23 -0800

    add UserPhoto to User component

--- src/components/shared/User.js
+++ src/components/shared/User.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import UserPhoto from './UserPhoto';
 
-export const User = ({ displayName, uid, children }) => (
-  <Link to={`/u/${uid}`}>{children || displayName || uid}</Link>
-);
+export const User = ({ children, displayName, size = 24, uid }) => {
+  if (!uid) {
+    console.error('User component requires value for uid');
+  }
+
+  let content = uid; //default
+  if (children) {
+    content = children; //priority
+  } else if (uid && displayName) {
+    content = (
+      <>
+        <UserPhoto uid={uid} size={size} />
+        <span className="ml-1">{displayName}</span>
+      </>
+    );
+  }
+
+  return (
+    <Link to={`/u/${uid}`} style={{ whiteSpace: 'nowrap' }}>
+      {content}
+    </Link>
+  );
+};
