a29e9d63a241100dd88cefa681b4c99edb16678e
geoffreyhale <geoffreyhale@gmail.com>
2021-02-15 14:22:34 -0800

    new post Visibility

--- src/components/views/Posts/NewTopLevelPostCard.js
+++ src/components/views/Posts/NewTopLevelPostCard.js
@@ -1,10 +1,23 @@
 import React, { useContext } from 'react';
 import Card from 'react-bootstrap/Card';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import NewPostForm from './NewPostForm';
 import { AppContext } from '../../AppProvider';
 import { createPost } from '../../../api/index';
 import { UserPhoto } from '../../shared/User';
 import { PostHeaderRoom } from './Post';
+
+const Visibility = () => (
+  <OverlayTrigger
+    placement="right"
+    overlay={<Tooltip>Visibility: Logged In Users</Tooltip>}
+  >
+    <small className="text-muted">
+      <i class="fas fa-user-check"></i>
+    </small>
+  </OverlayTrigger>
+);
 
 const NewTopLevelPostCard = ({ hackRoom }) => {
   const { user } = useContext(AppContext);
@@ -18,11 +31,19 @@ const NewTopLevelPostCard = ({ hackRoom }) => {
             <PostHeaderRoom room={hackHackRoom} />
           </div>
         )}
-        <Card.Title>
-          <UserPhoto uid={user.uid} />
-          <span className="ml-2">{user.displayName}</span>
-          <small className="text-muted ml-2">&#127757; Public</small>
-        </Card.Title>
+        <div>
+          <div style={{ display: 'inline-block', verticalAlign: 'top' }}>
+            <UserPhoto uid={user.uid} />
+          </div>
+          <div className="ml-2" style={{ display: 'inline-block' }}>
+            <h5 className="mb-1" style={{ marginBottom: 0 }}>
+              {user.displayName}
+            </h5>
+            <div>
+              <Visibility />
+            </div>
+          </div>
+        </div>
         <NewPostForm
           onSubmit={createPost}
           multiline={true}
