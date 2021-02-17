4d43167aba0a8b88a04f0a7c18ef843f366636db
geoffreyhale <geoffreyhale@gmail.com>
2021-02-16 17:30:16 -0800

    UserModality

--- src/components/views/Training/index.js
+++ src/components/views/Training/index.js
@@ -1,10 +1,101 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
+import { getPosts } from '../../../api';
 import { AppContext } from '../../AppProvider';
+import Spinner from '../../shared/Spinner';
+import { UserPhoto } from '../../shared/User';
 import MODALITIES from '../Posts/Modality/MODALITIES';
+
+//TODO this calls getPosts every time the selected modality changes and rerenders this
+const UserModality = () => {
+  const { user, modality: contextModalityKey } = useContext(AppContext);
+
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState({});
+
+  useEffect(() => {
+    getPosts((posts) => {
+      const userModalityPosts = Object.values(posts).filter(
+        (post) =>
+          post.userId === user.uid &&
+          post.modality &&
+          post.modality.name === contextModalityKey
+      );
+      setPosts(userModalityPosts);
+      setLoading(false);
+    });
+  }, []);
+
+  const userModalityPostsCount =
+    posts && typeof posts === 'object' && Object.keys(posts).length;
+  const [userModalityYesCount, userModalityNoCount] =
+    posts &&
+    typeof posts === 'object' &&
+    Object.values(posts).reduce(
+      ([yesCount, noCount], post) => [
+        yesCount + post.modality && post.modality.votes
+          ? Object.values(post.modality.votes).filter((vote) => vote === true)
+              .length
+          : 0,
+        noCount + post.modality && post.modality.votes
+          ? Object.values(post.modality.votes).filter((vote) => vote === false)
+              .length
+          : 0,
+      ],
+      [0, 0]
+    );
+
+  // const userModalityVoteCount = userModalityYesCount + userModalityNoCount;
+  // const userModalityYesPercentage = Math.round(
+  //   (userModalityYesCount / userModalityVoteCount) * 100
+  // );
+  // const userModalityNoPercentage = Math.round(
+  //   (userModalityNoCount / userModalityVoteCount) * 100
+  // );
+
+  return (
+    <ListGroup horizontal className="my-3">
+      <ListGroup.Item>
+        <UserPhoto uid={user.uid} />
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <div>
+          <strong>Posts</strong>
+        </div>
+        <div style={{ textAlign: 'center' }}>
+          {loading ? <Spinner /> : userModalityPostsCount}
+        </div>
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <div>
+          <strong>Yes</strong>
+        </div>
+        <div style={{ textAlign: 'center' }}>
+          {loading ? (
+            <Spinner />
+          ) : (
+            `${userModalityYesCount}` // (${userModalityYesPercentage}%)`
+          )}
+        </div>
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <div>
+          <strong>No</strong>
+        </div>
+        <div style={{ textAlign: 'center' }}>
+          {loading ? (
+            <Spinner />
+          ) : (
+            `${userModalityNoCount}` // (${userModalityNoPercentage}%)`
+          )}
+        </div>
+      </ListGroup.Item>
+    </ListGroup>
+  );
+};
 
 const Modalities = () => {
   const { modality: contextModalityKey, setModality } = useContext(AppContext);
@@ -54,6 +145,7 @@ const Modalities = () => {
             {modalityToShow && (
               <>
                 <h2>{modalityToShow.title}</h2>
+                <UserModality key={contextModalityKey} />
                 {modalityToShow.description}
               </>
             )}
