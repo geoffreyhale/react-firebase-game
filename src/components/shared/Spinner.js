e0687eb024439722235e76f0af6d1cb3b35592dc
geoffreyhale <geoffreyhale@gmail.com>
2021-02-16 20:41:13 -0800

    Spinner passes size prop through

--- src/components/shared/Spinner.js
+++ src/components/shared/Spinner.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import BootstrapSpinner from 'react-bootstrap/Spinner';
 
-const Spinner = () => (
-  <BootstrapSpinner animation="border" role="status" variant="primary">
+const Spinner = ({ size }) => (
+  <BootstrapSpinner
+    animation="border"
+    role="status"
+    variant="primary"
+    size={size}
+  >
     <span className="sr-only">Loading...</span>
   </BootstrapSpinner>
 );
