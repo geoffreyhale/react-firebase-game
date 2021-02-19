b79379be65a9a964e697b038fbb36077bdb630bf
geoffreyhale <geoffreyhale@gmail.com>
2021-02-18 23:53:04 -0800

    PostsPerDayBarChart

--- src/components/views/Admin/Posts.js
+++ src/components/views/Admin/Posts.js
@@ -1,4 +1,5 @@
 import format from 'date-fns/format';
+import { ResponsiveBar } from '@nivo/bar';
 import React from 'react';
 import Table from 'react-bootstrap/Table';
 import countWords from '../../shared/countWords';
@@ -78,9 +79,82 @@ export const PostsMiniCard = ({ posts }) => {
   );
 };
 
+const PostsPerDayBarChart = ({ postsByDay }) => {
+  const barData = [];
+  Object.entries(postsByDay).forEach(([day, posts]) => {
+    barData.push({
+      day: format(dateFromDaysSinceEpoch(day), 'MMMM d, yyyy'),
+      postsCount: Object.keys(posts).length,
+    });
+  });
+
+  return (
+    <div style={{ height: 400 }}>
+      <ResponsiveBar
+        data={Object.values(barData)}
+        keys={['postsCount']}
+        indexBy="day"
+        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+        padding={0.3}
+        valueScale={{ type: 'linear' }}
+        indexScale={{ type: 'band', round: true }}
+        colors={{ scheme: 'nivo' }}
+        borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'days',
+          legendPosition: 'middle',
+          legendOffset: 32,
+        }}
+        axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'posts',
+          legendPosition: 'middle',
+          legendOffset: -40,
+        }}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+        legends={[
+          {
+            dataFrom: 'keys',
+            anchor: 'bottom-right',
+            direction: 'column',
+            justify: false,
+            translateX: 120,
+            translateY: 0,
+            itemsSpacing: 2,
+            itemWidth: 100,
+            itemHeight: 20,
+            itemDirection: 'left-to-right',
+            itemOpacity: 0.85,
+            symbolSize: 20,
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+      />
+    </div>
+  );
+};
+
 export const PostsPerDay = ({ posts }) => {
   const postsByDay = {};
-
   Object.values(posts).forEach((post) => {
     const day = daysSinceEpoch(post.timestamp);
     postsByDay[day] = postsByDay[day] || {};
@@ -88,17 +162,20 @@ export const PostsPerDay = ({ posts }) => {
   });
 
   return (
-    <Table>
-      <tbody>
-        {Object.entries(postsByDay).map(([daysSinceEpoch, posts]) => (
-          <tr>
-            <td>
-              {format(dateFromDaysSinceEpoch(daysSinceEpoch), 'MMMM d, yyyy')}{' '}
-            </td>
-            <td>{Object.keys(posts).length}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+    <>
+      <PostsPerDayBarChart postsByDay={postsByDay} />
+      <Table>
+        <tbody>
+          {Object.entries(postsByDay).map(([daysSinceEpoch, posts]) => (
+            <tr>
+              <td>
+                {format(dateFromDaysSinceEpoch(daysSinceEpoch), 'MMMM d, yyyy')}{' '}
+              </td>
+              <td>{Object.keys(posts).length}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
   );
 };
