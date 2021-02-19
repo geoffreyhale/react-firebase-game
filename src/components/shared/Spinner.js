6d4c0c601b18d321a5e8e5bcd5b9dc0cafb2c5fe
geoffreyhale <geoffreyhale@gmail.com>
2021-02-18 22:08:29 -0800

    add Spinner size "lg"

--- src/components/shared/Spinner.js
+++ src/components/shared/Spinner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import BootstrapSpinner from 'react-bootstrap/Spinner';
 
-const Spinner = ({ size }) => (
+const SpinnerCore = ({ size }) => (
   <BootstrapSpinner
     animation="border"
     role="status"
@@ -11,5 +11,14 @@ const Spinner = ({ size }) => (
     <span className="sr-only">Loading...</span>
   </BootstrapSpinner>
 );
+
+const Spinner = ({ size }) =>
+  size === 'lg' ? (
+    <div style={{ textAlign: 'center', fontSize: '10rem' }}>
+      <SpinnerCore />
+    </div>
+  ) : (
+    <SpinnerCore size={size} />
+  );
 
 export default Spinner;
