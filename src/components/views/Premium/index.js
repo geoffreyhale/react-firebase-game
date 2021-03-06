bd667cdcf752038afe1330b48348cb26cf02b87e
geoffreyhale <geoffreyhale@gmail.com>
2021-03-06 10:26:43 -0800

    PremiumSaleCard inline-block

--- src/components/views/Premium/index.js
+++ src/components/views/Premium/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Card from 'react-bootstrap/Card';
 import { PremiumSaleCard } from '../../shared/Premium';
+import { AppContext } from '../../AppProvider';
+import friendlyTimestamp from '../../shared/friendlyTimestamp';
+
+const UserPremium = () => {
+  const { user } = useContext(AppContext);
+  return (
+    <span>
+      {user.isPremium ? (
+        <>
+          You have premium until{' '}
+          {friendlyTimestamp(user.premium.seconds * 1000)}.
+        </>
+      ) : (
+        'You do not have premium.'
+      )}
+    </span>
+  );
+};
 
 const PremiumPage = () => {
   return (
     <Card>
       <Card.Body>
         <Card.Title>Premium</Card.Title>
-        <PremiumSaleCard />
+        <Card>
+          <Card.Body>
+            <UserPremium />
+          </Card.Body>
+        </Card>
       </Card.Body>
     </Card>
   );
