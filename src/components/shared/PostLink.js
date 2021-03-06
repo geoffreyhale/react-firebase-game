2cd3079a8774aa33fa0d94bc8fda1c642f83fe29
geoffreyhale <geoffreyhale@gmail.com>
2021-03-05 19:43:16 -0800

    postLink

--- src/components/shared/PostLink.js
+++ src/components/shared/PostLink.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+export const postLink = ({ id, room }, includeHost = false) =>
+  `${includeHost ? window.location.host : ''}/r/${room}/posts/${id}`;
+
 const PostLink = ({ children, id, room }) => (
-  <Link to={`/r/${room}/posts/${id}`}>{children}</Link>
+  <Link to={postLink({ id, room })}>{children}</Link>
 );
 export default PostLink;
