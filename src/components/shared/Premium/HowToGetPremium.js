53a2dde4d6366b0dac5423d752eedecb107bf8b2
geoffreyhale <geoffreyhale@gmail.com>
2021-03-07 09:54:47 -0800

    Get Premium

--- src/components/shared/Premium/HowToGetPremium.js
+++ src/components/shared/Premium/HowToGetPremium.js
@@ -1,21 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const HowToGetPremium = () => (
-  <>
-    <h6>One Year Premium:</h6>
-    <p>
-      Send $30 to geoffreyhale@gmail.com via{' '}
-      <a href="paypal.com" target="_blank">
-        PayPal
-      </a>
-      .
-      <ul>
-        <li>"Sending to a friend"</li>
-        <li>
-          Add a note: "{'<'}youremail@gmail.com{'>'} premium"
-        </li>
-      </ul>
-    </p>
-  </>
-);
+const HowToGetPremium = () => <Link to="/premium">Get Premium</Link>;
 export default HowToGetPremium;
