243d0f9d081bc615401da92fde0d76006ca7b522
geoffreyhale <geoffreyhale@gmail.com>
2021-03-08 15:41:31 -0800

    events table

--- src/components/views/Events/index.js
+++ src/components/views/Events/index.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
+import Table from 'react-bootstrap/Table';
+import { Link, useHistory } from 'react-router-dom';
 import { getEvents } from '../../../api';
 import Spinner from '../../shared/Spinner';
 
 const EventsPage = () => {
+  const history = useHistory();
   const [events, setEvents] = useState({});
   const [loading, setLoading] = useState(true);
 
@@ -19,11 +21,31 @@ const EventsPage = () => {
 
   return (
     <Card>
-      <Card.Title>Events</Card.Title>
-      {events &&
-        Object.values(events).map((event) => (
-          <Link to={`/events/${event.id}`}>{event.title}</Link>
-        ))}
+      <Card.Body>
+        <Card.Title>Events</Card.Title>
+        <Table hover>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Location</th>
+            </tr>
+          </thead>
+          <tbody>
+            {events &&
+              Object.values(events).map((event) => {
+                const url = `/events/${event.id}`;
+                return (
+                  <tr onClick={() => history.push(url)}>
+                    <td>
+                      <Link to={url}>{event.title}</Link>
+                    </td>
+                    <td>{event.location}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </Table>
+      </Card.Body>
     </Card>
   );
 };
