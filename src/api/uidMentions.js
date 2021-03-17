e090bc437aefd5b139667176b54237619c1ef6fd
geoffreyhale <geoffreyhale@gmail.com>
2021-03-17 16:09:16 -0700

    improve uidMentions with positive lookbehind @

--- src/api/uidMentions.js
+++ src/api/uidMentions.js
@@ -1,6 +1,4 @@
 //TODO tests for this regex
 const uidMentions = (postContent) =>
-  postContent
-    .match(/@[0-9a-zA-Z]{28}(?![0-9a-zA-Z])/g)
-    ?.map((uid) => uid.substring(1)) || null;
+  postContent.match(/(?<=@)[0-9a-zA-Z]{28}(?![0-9a-zA-Z])/g);
 export default uidMentions;
