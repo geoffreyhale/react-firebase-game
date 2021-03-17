b4a217eabe633ddc493a8e553b092c05718b0b60
geoffreyhale <geoffreyhale@gmail.com>
2021-03-17 16:04:40 -0700

    uidMentions works adjacent to non alphanumeric characters

--- src/api/uidMentions.test.js
+++ src/api/uidMentions.test.js
@@ -5,6 +5,8 @@ describe('uidMentions', () => {
     expect(uidMentions('')).toEqual(null);
     expect(uidMentions('@1aB2cD3eF4gH5hI6jK7lM8nO9pQ')).toEqual(null);
     expect(uidMentions('1aB2cD3eF4gH5hI6jK7lM8nO9pQ0')).toEqual(null);
+  });
+  it('returns null for @toomanyalphanumericcharacters', () => {
     expect(uidMentions('@4aB2cD3eF4gH5hI6jK7lM8nO9pQ000000')).toEqual(null);
   });
   it('returns array of results for valid matches', () => {
@@ -21,7 +23,7 @@ describe('uidMentions', () => {
       '@2aB2cD3eF4gH5hI6jK7lM8nO9pQ0',
     ]);
   });
-  it.skip('works adjacent to non alphanumeric symbols', () => {
+  it('works adjacent to non alphanumeric characters', () => {
     expect(uidMentions('(@1aB2cD3eF4gH5hI6jK7lM8nO9pQ0)')).toEqual([
       '@1aB2cD3eF4gH5hI6jK7lM8nO9pQ0',
     ]);
