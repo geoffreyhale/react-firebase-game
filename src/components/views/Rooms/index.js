8c677deea516ecea48ef3be8922423bfd121c3cb
geoffreyhale <geoffreyhale@gmail.com>
2021-03-31 14:11:06 -0700

    add sorting to Rooms table

--- src/components/views/Rooms/index.js
+++ src/components/views/Rooms/index.js
@@ -4,13 +4,40 @@ import Card from 'react-bootstrap/Card';
 import Table from 'react-bootstrap/Table';
 import { AppContext } from '../../AppProvider';
 import { getPosts } from '../../../api';
+import Spinner from '../../shared/Spinner';
+
+const sortByTitle = (a, b) =>
+  a.title.localeCompare(b.title, undefined, {
+    ignorePunctuation: true,
+  });
+const sortByPostCount = (a, b) => b.posts?.length - a.posts?.length;
+const sortByRequires = (a, b) =>
+  a.requires?.join().localeCompare(b.requires?.join(), undefined, {
+    ignorePunctuation: true,
+  });
 
 const Rooms = () => {
   const { rooms } = useContext(AppContext);
-  const [posts, setPosts] = useState({});
+  const [sortFunction, setSortFunction] = useState(() => sortByTitle);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    getPosts((posts) => setPosts(posts));
+    getPosts((posts) => {
+      Object.entries(rooms).forEach(([key, room]) => {
+        if (room.id === 'home') {
+          rooms[key].posts = { length: Object.values(posts).length };
+        } else {
+          rooms[key].posts = Object.values(posts).filter(
+            (post) => post.room === room.id
+          );
+        }
+      });
+      setLoading(false);
+    });
   }, []);
+
+  if (loading) <Spinner />;
+
   return (
     <Card>
       <Card.Body>
@@ -19,31 +46,24 @@ const Rooms = () => {
           <Table>
             <thead>
               <tr>
-                <th>Name</th>
-                <th>Posts</th>
-                <th>Requires</th>
+                <th onClick={() => setSortFunction(() => sortByTitle)}>Name</th>
+                <th onClick={() => setSortFunction(() => sortByPostCount)}>
+                  Posts
+                </th>
+                <th onClick={() => setSortFunction(() => sortByRequires)}>
+                  Requires
+                </th>
               </tr>
             </thead>
             <tbody>
               {Object.values(rooms)
-                .sort((a, b) =>
-                  a.title.localeCompare(b.title, undefined, {
-                    ignorePunctuation: true,
-                  })
-                )
+                .sort(sortFunction)
                 .map((room) => (
                   <tr>
                     <td>
                       <Link to={room.url}>{room.title}</Link>
                     </td>
-                    <td>
-                      {
-                        // TODO more efficient to do this once for each room instead of every render
-                        Object.values(posts).filter(
-                          (post) => post.room === room.id
-                        ).length
-                      }
-                    </td>
+                    <td>{room.posts?.length}</td>
                     <td>
                       {room.requires?.map((requirement, i, arr) => {
                         const R =
